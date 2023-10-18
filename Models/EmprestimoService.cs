b726461acb0fc925e335425c27fd6e68a6de4b11
graciele boeck <alessandraboeck72@gmail.com>
2023-10-18 15:37:01 -0300

    Correções

--- Models/EmprestimoService.cs
+++ Models/EmprestimoService.cs
@@ -35,7 +35,35 @@ namespace Biblioteca.Models
         {
             using(BibliotecaContext bc = new BibliotecaContext())
             {
-                return bc.Emprestimos.Include(e => e.Livro).ToList();
+                IQueryable<Emprestimo> query;
+
+                if(filtro != null)
+                {
+
+                        switch(filtro.TipoFiltro)
+                        {
+                            case "Usuario":
+                                query = bc.Emprestimos.Where(l => l.NomeUsuario.Contains(filtro.Filtro));
+                            break;
+
+                            case "Livro":
+                                query = bc.Emprestimos.Where(l => l.Livro.Titulo.Contains(filtro.Filtro));
+                            break;
+
+                            default:
+                                query = bc.Emprestimos;
+                            break;
+                        }
+                }
+                else
+                {
+                    // caso filtro não tenha sido informado
+                    query = bc.Emprestimos;
+                }
+                
+                //ordenação padrão
+                //return query.OrderBy(l => l.Titulo).ToList();
+                return query.Include(e => e.Livro).ToList();
             }
         }
 
